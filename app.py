46c48f8c199964689a001662ae96d0ba1e6c5b20
William Jackson <william@subtlecoolness.com>
2020-05-14 07:46:56 -0500

    Add /present and /cleanup endpoints

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import boto3
 import flask
 import logging
 import os
@@ -26,6 +27,64 @@ class Settings:
 
 settings = Settings()
 app = flask.Flask(__name__)
+
+
+@app.route('/present', methods=['POST'])
+def present():
+    data = flask.request.json
+    fqdn = data.get('fqdn')
+    value = data.get('value')
+    app.logger.info(f'Presenting: {fqdn} IN TXT {value}')
+    client = boto3.client('route53')
+    for hosted_zone_id in settings.hosted_zone_ids:
+        client.change_resource_record_sets(
+            HostedZoneId=hosted_zone_id,
+            ChangeBatch={
+                'Changes': [
+                    {
+                        'Action': 'UPSERT',
+                        'ResourceRecordSet': {
+                            'Name': fqdn,
+                            'Type': 'TXT',
+                            'TTL': 30,
+                            'ResourceRecords': [
+                                {'Value': value}
+                            ]
+                        }
+                    }
+                ]
+            }
+        )
+    return 'OK'
+
+
+@app.route('/cleanup', methods=['POST'])
+def cleanup():
+    data = flask.request.json
+    fqdn = data.get('fqdn')
+    value = data.get('value')
+    app.logger.info(f'Cleaning up: {fqdn} IN TXT {value}')
+    client = boto3.client('route53')
+    for hosted_zone_id in settings.hosted_zone_ids:
+        client.change_resource_record_sets(
+            HostedZoneId=hosted_zone_id,
+            ChangeBatch={
+                'Changes': [
+                    {
+                        'Action': 'DELETE',
+                        'ResourceRecordSet': {
+                            'Name': fqdn,
+                            'Type': 'TXT',
+                            'TTL': 30,
+                            'ResourceRecords': [
+                                {'Value': value}
+                            ]
+                        }
+                    }
+                ]
+            }
+        )
+    return 'OK'
 
 
 def main():
